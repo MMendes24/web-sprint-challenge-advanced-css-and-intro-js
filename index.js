fab9519e4fc52311ac645c7a43b9dcae00a28c17
MMendes24 <mmbusiness1@protonmail.com>
2020-06-12 12:26:37 -0400

    Decided that stretch goal 2 sounded amazing. Learned about Fisher-Yates shuffle. Created an algorithim to sort my array randomly. Definitely one of the coolest things I've ever seen.

--- index.js
+++ index.js
@@ -302,8 +302,6 @@ function lotsOfArt(array){
 }
 
 
-
-
 // 🎨🎨 STRETCH 🎨🎨//
 
 
@@ -336,11 +334,15 @@ function getHTML(/* Code here */){
 
 /* STRETCH 2: Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
 
-    /* Code here */
-
+function randomize(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+  console.log(array)
+}
 
+randomize(artists)
 
  /* STRETCH 3: Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
